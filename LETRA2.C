bef90e2e746687761d51556aa8f9a527ce540098
fernando475 <fernandomatos22julia@gmail.com>
2024-10-21 11:58:00 -0300

    Add files via upload

---
+++ LETRA2.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+printf("informe a letra");
+char letra;
+scanf("%c",&letra);
+if(letra == 's'|| letra == 'S'){
+ printf("solteiro");
+}
+
+if(letra == 'c'|| letra == 'C'){
+ printf("casado");
+}
+
+if(letra == 'd'|| letra== 'D'){
+ printf("divorciado");
+}
+if(letra == 'v'|| letra == 'V'){
+ printf("viuvo");
+}
+
+
+
+
+
+
+
+
+}
